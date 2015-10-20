3b30482be4092902ae515a0b390365140142b29b
Martin Skorupski <martin.skorupski@highstreet-technologies.com>
2015-10-20 17:46:21 +0200

    init maven project for calculator

---
+++ calculator/src/main/java/com/highstreet/technologies/open/source/microwave/radio/link/calculator/App.java
@@ -0,0 +1,13 @@
+package com.highstreet.technologies.open.source.microwave.radio.link.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
